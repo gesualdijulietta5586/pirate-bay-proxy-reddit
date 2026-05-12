cf9853c38c7ff40c82e64f232c3807581ca5e679
gesualdijulietta5586 <gesualdijulietta@gmail.com>
2026-05-12 12:41:34 -0700

    Create main.cs

---
+++ main.cs
@@ -0,0 +1,11 @@
+// MIT License
+// Copyright (c) 2026 gesualdijulietta5586
+
+
+using System;
+
+class Program {
+    static void Main() {
+        Console.WriteLine("Windows Activation Education Pack placeholder");
+    }
+}
